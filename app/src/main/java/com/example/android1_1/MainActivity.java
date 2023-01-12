df5baf2d48ecdf94480143128fd34b9759372d1e
DanDdl <dangladste@gmail.com>
2023-01-12 17:35:20 +0200

    style: formatting

--- app/src/main/java/com/example/android1_1/MainActivity.java
+++ app/src/main/java/com/example/android1_1/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.android1_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements TextWatcher {
     private TextView anagramTextView;
@@ -37,13 +37,15 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     @Override
     public void afterTextChanged(Editable editable) {
-        String anagram = AnagramCreator.calculateAnagram(inputEditText.getText().toString(), filterEditText.getText().toString().trim());
-        if (!anagram.isEmpty()) {
-            anagramTextView.setTextAppearance(R.style.anagram_text);
-            anagramTextView.setText(anagram);
-        } else {
+        String anagram = AnagramCreator.calculateAnagram(
+                inputEditText.getText().toString(), filterEditText.getText().toString().trim()
+        );
+        if (anagram.isEmpty()) {
             anagramTextView.setTextAppearance(R.style.anagram_text_def);
             anagramTextView.setText(getString(R.string.anagram_text_def));
+        } else {
+            anagramTextView.setTextAppearance(R.style.anagram_text);
+            anagramTextView.setText(anagram);
         }
     }
 }
