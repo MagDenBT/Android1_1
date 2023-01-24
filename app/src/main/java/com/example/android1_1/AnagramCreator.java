4940da234aa256697c5eeb12075ce6472ffa5e9d
Bilto <magdenbt@mail.ru>
2023-01-24 13:20:47 +0300

    Improvement #1 - Improved application fault tolerance

--- app/src/main/java/com/example/android1_1/AnagramCreator.java
+++ app/src/main/java/com/example/android1_1/AnagramCreator.java
@@ -3,37 +3,40 @@ package com.example.android1_1;
 public class AnagramCreator {
 
     public static String calculateAnagram(String sourceText, String filter) {
+        try {
+            if (sourceText.trim().isEmpty()) {
+                return "";
+            }
 
-        if (sourceText.trim().isEmpty()) {
-            return "";
-        }
+            final char[] text = sourceText.toCharArray();
+            final int end = text.length - 1;
+            int startIdx = -1;
+            for (int i = 0; i <= end; i++) {
+                if (startIdx == -1) {
+                    if (Character.isSpaceChar(text[i])) {
+                        continue;
+                    } else {
+                        startIdx = i;
+                    }
+                }
 
-        final char[] text = sourceText.toCharArray();
-        final int end = text.length - 1;
-        int startIdx = -1;
-        for (int i = 0; i <= end; i++) {
-            if (startIdx == -1) {
-                if (Character.isSpaceChar(text[i])) {
-                    continue;
-                } else {
-                    startIdx = i;
+                if (i == end || Character.isSpaceChar(text[i])) {
+                    int endInd = i == end ? i : i - 1;
+                    reverse(text, startIdx, endInd, filter);
+                    startIdx = -1;
                 }
             }
-
-            if (i == end || Character.isSpaceChar(text[i])) {
-                int endInd = i == end ? i : i - 1;
-                reverse(text, startIdx, endInd, filter);
-                startIdx = -1;
-            }
+            return new String(text);
+        } catch (Exception e) {
+            return "";
         }
-        return new String(text);
     }
 
     protected static void reverse(char[] word, int startIdx, int endIdx, String filter) {
         while (startIdx < endIdx) {
-            if (fitsFilter(filter, word[startIdx])){
+            if (fitsFilter(filter, word[startIdx])) {
                 startIdx++;
-            }else if (fitsFilter(filter, word[endIdx])) {
+            } else if (fitsFilter(filter, word[endIdx])) {
                 endIdx--;
             } else {
                 char tmp = word[startIdx];
@@ -44,7 +47,8 @@ public class AnagramCreator {
             }
         }
     }
-    private static  boolean fitsFilter(String filter, char sym){
-        return (filter.trim().isEmpty() && !Character.isAlphabetic(sym)) || filter.indexOf(sym) > -1 ;
+
+    private static boolean fitsFilter(String filter, char sym) {
+        return (filter.trim().isEmpty() && !Character.isAlphabetic(sym)) || filter.indexOf(sym) > -1;
     }
 }
